e4418b892503db200c455fc22431b5884ff9b103
KittyChen913 <nightmare.kitty111@gmail.com>
2025-12-23 18:01:23 +0800

    feat: add SSH command output for connecting to private instance via Bastion

--- main.tf
+++ main.tf
@@ -212,3 +212,11 @@ output "ssh_command" {
   value       = "ssh -i ~/.ssh/terraform-ec2 ec2-user@${aws_instance.public.public_ip}"
   description = "連接到 public instance 的 SSH 命令"
 }
+
+# Output：輸出透過 Bastion 連接 Private Instance 的 SSH 命令
+output "ssh_command_via_bastion" {
+  value       = <<-EOT
+ssh -o "ProxyCommand=ssh -i ~/.ssh/terraform-ec2 -W %h:%p ec2-user@${aws_instance.public.public_ip}" -i ~/.ssh/terraform-ec2 ec2-user@${aws_instance.private.private_ip}
+EOT
+  description = "使用 ProxyCommand 連接到 private instance 的 SSH 命令"
+}
